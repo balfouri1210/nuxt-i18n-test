3db6cdf19570f4eb96508e6be3bd40f755aca866
jonghoon kim <balfouri1210@gmail.com>
2022-07-31 23:43:16 +0900

    Project setting

---
+++ pages/_brandName/_brandId.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ this.$t('welcome') }}</h1>
+
+    <h2>brandName: {{ this.$route.params.brandName }}</h2>
+    <h2>brandId: {{ this.$route.params.brandId }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <nuxt-link :to="localePath({
+    name: 'brandName-brandId',
+    params: {
+      brandName: 'apple',
+      brandId: 1
+    }
+  })" :style="{
+    fontSize: '28px'
+  }">브랜드페이지로</nuxt-link>
+  <Tutorial/>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
